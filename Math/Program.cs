a175b6df0756ab85b453345b229e6ba735b66321
Tin-Nguyen-Student <112492691+Tin-Nguyen-Student@users.noreply.github.com>
2023-01-25 15:00:09 +0000

    Saved Work 2

--- Math/Program.cs
+++ Math/Program.cs
@@ -1,9 +1,74 @@
-﻿public class Math
+﻿
+namespace MyUtilities
+
+{
+public class Math
 {
 
-        public static void Main{string[] args}
+        public static void MathUtils(string[] args)
         {
 
         }
         
+         public static void Power(int x, int y)
+        {
+                
+        }
+        public static void SquareRoot(int x)
+        {
+
+        }
+        
+         public static void AbsoluteValue(int x)
+        {
+                
+        }
+         public static void Min(int x, int y)
+        {
+                
+        }
+         public static void Max(int x, int y)
+        {
+                
+        }
+         public static void Add(int x, int y)
+        {
+                
+        }
+
+         public static void Subtract(int x, int y)
+        {
+                
+        }
+
+         public static void Divide(int x, int y)
+        {
+                
+        }
+
+         public static void Multiply(int x, int y)
+        {
+                
+        }
+
+         public static void Log(int x, int y)
+        {
+                
+        }
+
+         public static void Exponent(int x, int y)
+        {
+                
+        }
+
+         public static void Remainder(int x, int y)
+        {
+                DivRem
+        }
+
+         public static void Round(int x, int y)
+        {
+                
+        }
+    }
 }
