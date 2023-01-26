e5a024485f5f52cd72392fbbbe78f51f380be4fc
Jacob-Judge-Student <112559074+Jacob-Judge-Student@users.noreply.github.com>
2023-01-26 14:58:14 +0000

    i hate my life

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿public class String
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(ToUpper());
+    }
+    public string ToUpper()
+    {
+        string upperstr1 = "testing";
+        string testing = upperstr1.ToUpper();
+        return testing;
+    }
+
+    public static string ToLower()
+    {
+        str.ToLower () 
+    }
+
+
+    public static string Length
+    {
+
+    }
+
+
+    public static string Reverse
+    {
+
+    }
+
+
+    public static string CountVowels
+    {
+
+    }
+
+
+    public static string CountConsonants
+    {
+
+    }
+
+
+    public static string HasLetter
+    {
+
+    }
+
+}
+
