9e273a798c6e36a6fbb6107ffb9fc9c24feab7db
Tin-Nguyen-Student <112492691+Tin-Nguyen-Student@users.noreply.github.com>
2023-01-30 10:51:41 +0000

    Saved

--- Math/Program.cs
+++ Math/Program.cs
@@ -7,21 +7,35 @@ public class Math
 
         public static void MathUtils(string[] args)
         {
-
+                MathUtils.Power(6,5);
         }
         
-         public static void Power(int x, int y)
+         public static double Power(double x, double y)
         {
                 
+                for (int x = 0; x <= 50; x++)
+                {
+                        int answer = MathUtils.Power(x, y);
+                        Console.WriteLine(answer);
+                        
+                }
         }
-        public static void SquareRoot(int x)
+        public static double SquareRoot(int x)
         {
-
+                foreach (int i in x)
+                {
+                      int answer =  Math.SquareRoot(x);
+                      Console.WriteLine(answer);
+                }
         }
         
          public static void AbsoluteValue(int x)
         {
-                
+                foreach (int i in x)
+                {
+                      int answer = Math.AbsoluteValue(x);
+                      Console.WriteLine(answer);
+                }
         }
          public static void Min(int x, int y)
         {
@@ -33,22 +47,39 @@ public class Math
         }
          public static void Add(int x, int y)
         {
+                foreach (int i in x)
+                {
+                      int answer = x + y;
+                      Console.Writeline(answer);
+                }
                 
         }
 
          public static void Subtract(int x, int y)
         {
-                
+                foreach (int i in x)
+                {
+                        int answer= x - y;
+                        Console.WriteLine(answer);
+                }
         }
 
          public static void Divide(int x, int y)
         {
-                
+                foreach (int i in x)
+                {
+                        int answer = x / y;
+                        Console.WriteLine(answer);
+                }
         }
 
          public static void Multiply(int x, int y)
         {
-                
+                foreach (int i in x)
+                {
+                        int answer = x * y;
+                        Console.Writeline(answer);
+                }
         }
 
          public static void Log(int x, int y)
@@ -61,14 +92,17 @@ public class Math
                 
         }
 
-         public static void Remainder(int x, int y)
+         public static double Remainder(int x, int y)
         {
-                DivRem
+                double answer = x / y;
+                double remainder = Math.Remainder(answer);
+                Console.WriteLine(remainder);
         }
 
-         public static void Round(int x, int y)
+         public static double Round(int x)
         {
-                
+                double answer = Math.Round(x);
+                Console.Writeline(answer);
         }
     }
 }
---
+++ Tests/Program.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+
+public class Tests
+{
+
+    [Facts]
+
+    public void TestingHere()
+    {
+        Assert.Equal(4, MathUtils.Power(2, 2));
+    }
+}
